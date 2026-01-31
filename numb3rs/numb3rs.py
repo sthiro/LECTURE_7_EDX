c3e3a69a50b5a397eb00459f77c337da622bf6b8
Thiroshan Suthakar <thiroshansuthakar@Thiroshans-MacBook-Air.local>
2026-01-31 10:51:28 +0530

    git fix

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -9,7 +9,7 @@ def main():
 def validate(ip):
     ip = ip.strip()
     SINGLE_BLOCK = "(0|[1-9][0-9]?|1[0-9]{0,2}|2([0-4][0-9]?)?|25[0-5])" # Checks whether single block number is in range of 0-255
-    IPV4_PATTERN = rf"^{SINGLE_BLOCK}\.{SINGLE_BLOCK}\.{SINGLE_BLOCK}\.{SINGLE_BLOCK}$" #Checks whole IPV4 Pattern (4 individial blocks)
+    IPV4_PATTERN = rf"^{SINGLE_BLOCK}\.{SINGLE_BLOCK}\.{SINGLE_BLOCK}\.{SINGLE_BLOCK}$" # Checks whole IPV4 Pattern (4 individial blocks)
     match = re.search(IPV4_PATTERN, ip)
 
     if match: return True
