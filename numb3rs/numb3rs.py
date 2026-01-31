23d595a0866a0cff723ca2d9815d13a078745719
Thiroshan Suthakar <thiroshansuthakar@Thiroshans-MacBook-Air.local>
2026-01-31 10:56:02 +0530

    Tested numb3rs manually and using unit testing, works perfectly

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -10,6 +10,7 @@ def validate(ip):
     ip = ip.strip()
     SINGLE_BLOCK = "(0|[1-9][0-9]?|1[0-9]{0,2}|2([0-4][0-9]?)?|25[0-5])" # Checks whether single block number is in range of 0-255
     IPV4_PATTERN = rf"^{SINGLE_BLOCK}\.{SINGLE_BLOCK}\.{SINGLE_BLOCK}\.{SINGLE_BLOCK}$" # Checks whole IPV4 Pattern (4 individial blocks)
+    # Note : rf => Raw (r) string + (f) string, (fr) will work too
     match = re.search(IPV4_PATTERN, ip)
 
     if match: return True
