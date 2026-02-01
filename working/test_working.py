d8d70cb8fb75f183758726d8cfa34000073078f4
Thiroshan Suthakar <thiroshansuthakar@Thiroshans-MacBook-Air.local>
2026-02-01 21:35:14 +0530

    made unit testing code. Now i need to test using cs50 tester

--- working/test_working.py
+++ working/test_working.py
@@ -1,1 +1,23 @@
-from 
+from working import convert
+import pytest
+
+
+def test_normal():
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("1 AM to 4:30 PM") == "01:00 to 16:30"
+    assert convert("12 AM to 12:00 PM") == "00:00 to 12:00"
+
+def test_raise_error():
+
+    with pytest.raises(ValueError):
+        convert("1 AM - 4:30 PM")
+
+    with pytest.raises(ValueError):
+        convert("1 : 4:30 PM")
+    
+    with pytest.raises(ValueError):
+        convert("1 AM : 4:3 PM")
+
+    with pytest.raises(ValueError):
+        convert("sample  Text")
+    
